dfa720b0c1c75dfa9c88745cbe6630a0d949e201
rohit rawat <rohitrawatrr07@gmail.com>
2025-09-15 15:35:37 +0530

    after app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 4
-        versionName = "1.0.3" 
+        versionCode = 7
+        versionName = "1.0.7" 
     }
  signingConfigs {
         create("release") {
